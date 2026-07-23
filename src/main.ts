6bf3c4d3c8b536e633da4be8946878980c07d002
Cesar Mantilla <nmantillaq@gmail.com>
2026-07-22 22:35:05 -0500

    update cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { METHODS } from 'http';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.enableCors({
+    origin: '*',
+    METHODS: '*'
+  });
+
+  await app.listen(process.env.PORT ?? 3000);  
+
 }
 bootstrap();
